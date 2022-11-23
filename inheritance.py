cf20fa71c8e9aceed42ef2c3bfa7966ea22a6afc
davdorado <david.dorado@ngny.tech>
2022-11-23 19:41:38 +0100

    Repaso clase de herencia
    incluye ejemplo de como pasar multiples valores a un seter usando tuplas

--- inheritance.py
+++ inheritance.py
@@ -12,6 +12,29 @@ class Square(Rectangle):
         #call parent class constructor
         super().__init__(side,side)
 
+class User:
+    def __init__(self,name,last_name,job_title):
+        self._name = name
+        self._last_name = last_name
+        self._job_title = job_title
+        self._email = ""
+
+    @property
+    def email(self):
+        return self._name + "." + self._last_name + "@mail.com"
+
+    @property
+    def user_name(self):
+        return self._name + " " + self._last_name
+
+    @user_name.setter
+    def set_user_name(self,full_name):
+        self._name = full_name[0]
+        self._last_name = full_name[1]
+
+class Engineer(User):
+    def __init__(self, name, last_name, job_title):
+        super().__init__(name, last_name, job_title)
 
 if __name__ == '__main__':
     rectangle = Rectangle(3,4)
@@ -20,3 +43,13 @@ if __name__ == '__main__':
     square = Square(8)
 
     print("Square area: " , square.area)
+
+    emp1 = Engineer("David","Dorado","Electronic")
+
+    print("Emp name " + emp1.user_name)
+    print("emai: " + emp1.email)
+
+    emp1.set_user_name = ("Francisco","Sevilla")
+
+    print("Emp name " + emp1.user_name)
+    print("emai: " + emp1.email)
